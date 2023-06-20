d054c18f9f95d4429ebc45e9d5d45575399beeff
ManalBen1 <manaltini@gmail.com>
2023-06-20 15:12:44 -0700

    files

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,14 @@
 #include "main.h"
-	/**
-	 * main - print _putchar
-	 * is written by mbah
-	 * Return: 0
-	 */
 
-
-	int main(void)
-	{
+/**
+ * main - Entry point
+ *
+ * Description: This function prints the characters of "_putchar"
+ *              followed by a new line.
+ * Return: 0 (Success)
+ */
+int main(void)
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -17,5 +18,6 @@
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
-	}
+}
