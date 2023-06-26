d49ea56966bde57ec8198eaa3152c62a2d64d5c5
ManalBen1 <manaltini@gmail.com>
2023-06-26 11:40:00 -0700

    files

---
+++ 0x05-pointers_arrays_strings/100-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+int _atoi(char *str); // Function declaration
+
+int main(void)
+{
+    int nb;
+
+    nb = _atoi("98");
+    printf("%d\n", nb);
+    // Rest of the code...
+}
