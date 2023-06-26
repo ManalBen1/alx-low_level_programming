ec63cc12057d5bd87a3c410ea92fe2a72144fa3c
ManalBen1 <manaltini@gmail.com>
2023-06-26 11:20:11 -0700

    files

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -1,19 +1,18 @@
-#ifndef MAIN_H
-#define MAIN_H
-
+#include "main.h"
 #include <stdio.h>
-#include <unistd.h>
 
-int _putchar(char c);
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
+/**
+ * main - check the code 
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int n;
 
-#endif
+    n = 402;
+    printf("n=%d\n", n);
+    reset_to_98(&n);
+    printf("n=%d\n", n);
+    return (0);
+}
