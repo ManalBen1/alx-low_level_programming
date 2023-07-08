1e057f42d128bd6c09505137da12834eda57e9d9
ManalBen1 <manaltini@gmail.com>
2023-07-07 17:06:51 -0700

    files

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -1,26 +1,30 @@
 #include "main.h"
 /**
-* _strstr - Entry point
-* @haystack: input
-* @needle: input
-* Return: Always 0 (Success)
-*/
+ * _strstr - Entry point
+ * @haystack: input
+ * @needle: input
+ * Return: Always 0 (Success)
+ */
 char *_strstr(char *haystack, char *needle)
 {
-for (; *haystack != '\0'; haystack++)
-{
-char *l = haystack;
-char *p = needle;
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *l = haystack;
+		char *p = needle;
 
-while (*l == *p && *p != '\0')
-{
-l++;
-p++;
+
+		while (*l == *p && *p != '\0')
+		{
+			l++;
+			p++;
+		}
+
+
+		if (*p == '\0')
+			return (haystack);
+	}
+
+
+	return (0);
 }
 
-if (*p == '\0')
-return (haystack);
-}
-
-return (0);
-}
