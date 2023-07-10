c0e23462eadade9e66fb10b6c3d0bf97da564e1d
ManalBen1 <manaltini@gmail.com>
2023-07-10 10:30:16 -0700

    files

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,22 +1,28 @@
-#include "main.h"
 #include <stdlib.h>
+
 /**
-* create_array - create array of size size and assign char c * @size: size of array
-* @c: char to assign
-* Description: create array of size size and assign char c
-* Return: pointer to array, NULL if fail
+* create_array - Creates an array of chars and initializes it
+*                with a specific char.
 *
+* @size: The size of the array.
+* @c: The character to initialize the array with.
+*
+* Return: On success, a pointer to the created array.
+*         Otherwise, NULL.
 */
 char *create_array(unsigned int size, char c)
 {
-char *str;
+if (size == 0)
+return NULL;
+
+char *array = NULL ;
+array= malloc(sizeof(char) * size);
+if (array == NULL)
+return NULL;
+
 unsigned int i;
-
-str = malloc(sizeof(char) * size);
-if (size == 0 || str == NULL)
-return (NULL);
-
 for (i = 0; i < size; i++)
-str[i] = c;
-return (str);
+array[i] = c;
+
+return array;
 }
--- 0x0B-malloc_free/0-main.c
+++ 0x0B-malloc_free/0-main.c
@@ -1,6 +1,7 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+
+char *create_array(unsigned int size, char c);
 
 /**
  * simple_print_buffer - prints buffer in hexa
@@ -40,12 +41,14 @@ int main(void)
     char *buffer;
 
     buffer = create_array(98, 'H');
-    if  (buffer == NULL)
+    if (buffer == NULL)
     {
-        printf("failed to allocate memory\n");
-        return (1);
+        printf("Failed to allocate memory\n");
+        return(1);
     }
+
     simple_print_buffer(buffer, 98);
     free(buffer);
-    return (0);
+return (0);
 }
+
