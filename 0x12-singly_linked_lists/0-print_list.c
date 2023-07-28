fe0b98db980dee9d5b4f70e40a35d26ef6932702
ManalBen1 <manaltini@gmail.com>
2023-07-28 10:50:35 -0700

    files

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,7 +5,7 @@
 
 /**
 * print_list - Prints all the elements of a list_t list.
-* @h: Pointer to the head of the list.
+* @head: Pointer to the head of the list.
 *
 * Return: The number of nodes in the list.
 */
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,12 +2,21 @@
 #define LISTS_H
 
 /* Definition of the structure */
+/**
+* struct list_s - singly linked list
+* Description: singly linked list node structure
+*@str: string - (malloc'ed string)
+* @len: length of the string
+* @next: points to the next node
+*/
+
 typedef struct list_s
 {
-    char *str;
-    int len;
-    struct list_s *next;
-} list_t;
+char *str;
+int len;
+struct list_s *next;
+}
+list_t;
 
 /* Function prototype */
 size_t print_list(const list_t *h);
