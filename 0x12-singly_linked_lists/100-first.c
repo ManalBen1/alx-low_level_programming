857b81572bf4737475cfb174a410eceb35814752
ManalBen1 <manaltini@gmail.com>
2023-07-28 13:20:49 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void print_before_main(void);
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+
+/**
+* main - check the code
+* Return: Always 0.
+*/
+int main(void)
+{
+printf("(A tortoise, having pretty good sense of a hare's nature,");
+printf("challenges one to a race.)\n");
+return (0);
+}
