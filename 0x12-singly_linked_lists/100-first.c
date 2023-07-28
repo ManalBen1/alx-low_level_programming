abcd9ad28ca504f63a94a70f618ed95b346d2829
ManalBen1 <manaltini@gmail.com>
2023-07-28 13:38:32 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-* print_before_main - Function that prints a message before main is executed. 
+* print_before_main - Function that prints a message before main is executed.
 * print_before_main -  Function that runs something before main is executed.
 */
 void print_before_main(void) __attribute__((constructor));
