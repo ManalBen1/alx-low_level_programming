3e5183711352509d6f7f69354818f9cd2dc293bf
ManalBen1 <manaltini@gmail.com>
2023-07-31 15:24:01 -0700

    6-pop_listint.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 #include <stddef.h>
+
 /**
 * struct listint_s - singly linked list
 * @n: integer
@@ -11,85 +12,88 @@ typedef struct listint_s
 {
 int n;
 struct listint_s *next;
-}
-listint_t;
+} listint_t;
+
 /**
 * print_listint - prints all the elements of a listint_t list
-* listint_len -  returns the number of elements in a linked listint_t list.
-* @h: Pointer to the head of the list.
+* @h: Pointer to the head of the list
 * Return: the number of nodes
 */
 size_t print_listint(const listint_t *h);
-size_t listint_len(const listint_t *h);
+
 /**
-* add_nodeint -  adds a new node at the beginning of a listint_t list.
-* @n: integer.
-* @h: Pointer to the head of the list.
-* Return: the address of the new element, or NULL if it failed.
+* listint_len - returns the number of elements in a linked listint_t list
+* @h: Pointer to the head of the list
+* Return: the number of nodes
+*/
+size_t listint_len(const listint_t *h);
+
+/**
+* add_nodeint - adds a new node at the beginning of a listint_t list
+* @h: Double pointer to the head of the list
+* @n: Integer value to be added
+* Return: the address of the new element, or NULL if it failed
 */
 listint_t *add_nodeint(listint_t **h, const int n);
+
+/**
+* add_nodeint_end - adds a new node at the end of a listint_t list
+* @h: Double pointer to the head of the list
+* @n: Integer value to be added
+* Return: the address of the new element, or NULL if it failed
+*/
 listint_t *add_nodeint_end(listint_t **h, const int n);
 
 /**
-*free_listint - frees a listint_t list.
-*@h: Pointer to the head of the list.
-*/ 
+* free_listint - frees a listint_t list
+* @h: Pointer to the head of the list
+*/
 void free_listint(listint_t *h);
 
 /**
-* free_listint2 - frees a listint_t list.
-*@h: Pointer to the head of the list.
-* @n: integer.
+* free_listint2 - frees a listint_t list
+* @h: Double pointer to the head of the list
 */
 void free_listint2(listint_t **h);
-/**
-*pop_listint -  deletes the head node of a listint_t linked list.
-*@h: Pointer to the head of the list.
-* @n: integer.
-*returns the head node’s data (n).
-*/
 
+/**
+* pop_listint - deletes the head node of a listint_t linked list
+* @h: Double pointer to the head of the list
+* Return: the head node's data (n) or 0 if the list is empty
+*/
 int pop_listint(listint_t **h);
 
 /**
-* get_nodeint_at_index - returns the nth node of a listint_t linked list.
-* where index is the index of the node, starting at 0
-* if the node does not exist, return NULL
+* get_nodeint_at_index - returns the nth node of a listint_t linked list
+* @h: Pointer to the head of the list
+* @index: Index of the node, starting at 0
+* Return: the nth node, or NULL if the node does not exist
 */
-
 listint_t *get_nodeint_at_index(listint_t *h, unsigned int index);
 
 /**
-* sum_listint -  returns the sum of all the data (n) of a listint_t linked list.
-* @h: Pointer to the head of the list.
-* if the list is empty, return 0
+* sum_listint - returns the sum of all the data (n) of a listint_t linked list
+* @h: Pointer to the head of the list
+* Return: the sum of all the data, or 0 if the list is empty
 */
-
 int sum_listint(listint_t *h);
 
 /**
-* insert_nodeint_at_index -  inserts a new node at a given position.
-* @n: integer.
-*@h: Pointer to the head of the list.
-* where idx is the index of the list where the new node should be added. Index starts at 0
-* Returns: the address of the new node, or NULL if it failed
-* if it is not possible to add the new node at index idx, do not add the new node and return NULL
+* insert_nodeint_at_index - inserts a new node at a given position
+* @h: Double pointer to the head of the list
+* @idx: Index of the new node to be inserted
+* @n: Integer value to be inserted
+* Return: the address of the new node, or NULL if it failed
 */
-
 listint_t *insert_nodeint_at_index(listint_t **h, unsigned int idx, int n);
 
 /**
-* delete_nodeint_at_index - deletes the node at index index of a listint_t linked list.
-* Returns: 1 if it succeeded, -1 if it failed
-* @h: Pointer to the head of the list.
+* delete_nodeint_at_index - deletes the node at index index
+* of a listint_t linked list
+* @h: Double pointer to the head of the list
+* @index: Index of the node to be deleted
+* Return: 1 if it succeeded, -1 if it failed
 */
-
-int delete_nodeint_at_index(listint_t **head, unsigned int index);
-
-
-
-
-
-
+int delete_nodeint_at_index(listint_t **h, unsigned int index);
 
 #endif /* LISTS_H */
