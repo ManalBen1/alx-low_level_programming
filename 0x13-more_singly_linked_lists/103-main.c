5a135e4a5026ad799ff5916a6989154e4c281477
ManalBen1 <manaltini@gmail.com>
2023-08-01 12:11:22 -0700

    103-find_loop.c

--- 0x13-more_singly_linked_lists/103-main.c
+++ 0x13-more_singly_linked_lists/103-main.c
@@ -11,25 +11,25 @@
 int main(void)
 {
     listint_t *h;
-    listint_t *head2;
+    listint_t *h2;
     listint_t *node;
 
-    head2 = NULL;
-    add_nodeint(&head2, 0);
-    add_nodeint(&head2, 1);
-    add_nodeint(&head2, 2);
-    add_nodeint(&head2, 3);
-    add_nodeint(&head2, 4);
-    add_nodeint(&head2, 98);
-    add_nodeint(&head2, 402);
-    add_nodeint(&head2, 1024);
-    print_listint_safe(head2);
-    node = find_listint_loop(head2);
+    h2 = NULL;
+    add_nodeint(&h2, 0);
+    add_nodeint(&h2, 1);
+    add_nodeint(&h2, 2);
+    add_nodeint(&h2, 3);
+    add_nodeint(&h2, 4);
+    add_nodeint(&h2, 98);
+    add_nodeint(&h2, 402);
+    add_nodeint(&h2, 1024);
+    print_listint_safe(h2);
+    node = find_listint_loop(h2);
     if (node != NULL)
     {
         printf("Loop starts at [%p] %d\n", (void *)node, node->n);
     }
-    free_listint_safe(&head2);
+    free_listint_safe(&h2);
     h = NULL;
     node = add_nodeint(&h, 0);
     add_nodeint(&h, 1);
