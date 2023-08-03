7da3468577e844b39593fc8991c39c21b206f37a
ManalBen1 <manaltini@gmail.com>
2023-08-03 02:29:16 -0700

    files

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,17 +14,16 @@ unsigned int binary_to_uint(const char *b)
 unsigned int n = 0;
 
 if (b == NULL)
-{
 return (0);
-}
 
 
-while (*b)
+while (*b != '\0')
 {
 if (*b != '0' && *b != '1')
 return (0);
-}
+
 n = (n << 1) + (*b - '0');
 b = b + 1;
+}
 return (n);
 }
