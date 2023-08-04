a0908b73d8779ea0eb9faeae7f33cee358270052
ManalBen1 <manaltini@gmail.com>
2023-08-03 18:17:27 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,7 +11,8 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 if (index >= (sizeof(unsigned long int) * 8))
+
 return (-1);
-return (n >> index);
-return (1);
+return ((n >> index)&1);
+
 }
