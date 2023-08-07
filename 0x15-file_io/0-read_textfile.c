92e219279aadd952bf6ea41a4ada82a7e19ccf52
ManalBen1 <manaltini@gmail.com>
2023-08-07 13:40:53 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,17 +3,53 @@
 #include "main.h"
 
 /**
-* read_textfile -  reads a text file and prints it to the POSIX standard output.
+* read_textfile - reads a text file and prints it to the POSIX standard output.
 * @letters: the number of letters it should read and print.
+* @filename: name of the fileto be read.
 * Returns: the actual number of letters it could read and print
-* if the file can not be opened or read, return 0
-* if filename is NULL return 0
-* if write fails or does not write the expected amount of bytes, return 0
+* Return: 0, if the file can not be opened or read, and if filename is NULL,
+* and  if write fails or does not write the expected amount of bytes.
 */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+
+ssize_t bytesR;
+
+char *buff;
+
+FILE *file;
+file = fopen(filename, "r");
+buff = malloc(letters + 1);
+bytesR = fread(buff, 1, letters, file);
+if (bytesR < 0)
+{
+free(buff);
+
+return (0);
+}
+if (!file)
+{
+return (0);
+}
 if (filename == NULL)
 {
 return (0);
+}
+
+
+if (buff == NULL)
+{
+fclose(file);
+
+return (0);
+}
+
+
+fclose(file);
+buff[bytesR] = '\0';
+printf("%s", buff);
+free(buff);
+
+return (bytesR);
 }
