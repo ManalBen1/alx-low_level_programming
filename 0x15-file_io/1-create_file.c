cea30fa710921bee489b79c5ba5205629074fa68
ManalBen1 <manaltini@gmail.com>
2023-08-08 15:33:17 -0700

    file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-int file;
+FILE *file;
 size_t len = 0;
 size_t written = 0;
 
@@ -26,8 +26,8 @@ if (filename == NULL)
 
 return (-1);
 
-file = fopen(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-if (file == -1)
+file = fopen(filename,"w");
+if (file == NULL)
 
 return (-1);
 
@@ -37,7 +37,7 @@ if (text_content != NULL)
 {
 while (text_content[len] != '\0')
 len = len + 1;
-written = fwrite(file, text_content, len);
+written = fwrite(text_content, sizeof(char), len, file);
 
 if (written != len)
 {
