3ac62ae571048e374bc0b1a326566194de1d9dbe
ManalBen1 <manaltini@gmail.com>
2023-08-07 17:52:10 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,8 +6,8 @@
 * read_textfile - reads a text file and prints it to the POSIX standard output.
 * @letters: the number of letters it should read and print.
 * @filename: name of the fileto be read.
-* Returns: the actual number of letters it could read and print
-* Return: 0, if the file can not be opened or read, and if filename is NULL,
+* Return: the actual number of letters it could read and print, and 0 if the
+* file can not be opened or read, and if filename is NULL,
 * and  if write fails or does not write the expected amount of bytes.
 */
 
