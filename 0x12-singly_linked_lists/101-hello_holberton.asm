f1156bff375506a4ddab0f64393cc4e84794b8fa
ManalBen1 <manatini@gmail.com>
2023-11-16 07:20:38 -0800

    file

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,1 +1,19 @@
-printf("Hello, Holberton,\n");
+section .data
+    hello_msg db 'Hello, Holberton', 10, 0  ; Message to be printed followed by newline
+
+section .text
+    global _start_func ; Custom entry point label
+
+    extern printf
+
+_start_func: 
+    ; Prepare arguments to call printf
+    mov edi, hello_msg  ; Load the address of the string into edi as the first argument
+    xor eax, eax        ; Clear eax register
+    call printf         ; Call the printf function
+
+    ; Exit the program
+    mov eax, 60         ; System call number for sys_exit
+    xor edi, edi        ; Exit code 0
+    syscall             ; Make the system call to exit
+
