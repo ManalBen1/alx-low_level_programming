8b374ffd480be2544851c7afa4af70e1c057772b
ManalBen1 <manatini@gmail.com>
2023-11-22 01:13:35 -0800

    files

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,17 +9,17 @@
 */
 size_t print_listint_safe(const listint_t *h)
 {
-size_t i = 0;
+size_t count = 0;
 const listint_t *slow_one = h;
 const listint_t *fast_one = h;
 
 if (h == NULL)
-return (0);
+return(0);
 
 while (fast_one && fast_one->next)
 {
 printf("[%p] %d\n", (void *)slow_one, slow_one->n);
-i = i + 1;
+count++;
 slow_one = slow_one->next;
 fast_one = fast_one->next->next;
 
@@ -27,12 +27,12 @@ if (slow_one == fast_one)
 {
 printf("[%p] %d\n", (void *)slow_one, slow_one->n);
 printf("Loop at [%p] %d\n", (void *)fast_one, fast_one->n);
-return(1);
+return(98);
 }
 }
 
 printf("[%p] %d\n", (void *)slow_one, slow_one->n);
-i = i + 1;
+count++;
 
-return (0);
+return (count);
 }
