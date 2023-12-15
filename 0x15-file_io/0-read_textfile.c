0d5debeacbc682f076cca2d721528444bd498efb
ManalBen1 <manatini@gmail.com>
2023-12-15 09:43:06 -0800

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,60 +1,36 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 
 /**
-* read_textfile - reads a text file and prints it to the POSIX standard output.
-* @letters: the number of letters it should read and print.
-* @filename: name of the fileto be read.
-* Return: the actual number of letters it could read and print, and 0 if the
-* file can not be opened or read, and if filename is NULL,
-* and  if write fails or does not write the expected amount of bytes.
+* read_textfile - reads a text file and prints the letters
+* @filename: filename.
+* @letters: numbers of letters printed.
+*
+* Return: numbers of letters printed. It fails, returns 0.
 */
-ssize_t read_textfile(const char *filename, size_t letters) {
-ssize_t bytesR;
-char *buff;
-FILE *file;
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+int fd;
+ssize_t nrd, nwr;
+char *buf;
 
-if (filename == NULL)
-{
-fprintf(stderr, "Filename is NULL\n");
-return 0;
-}
-file = fopen(filename, "r");
-if (!file) {
-fprintf(stderr, "Could not open file\n");
-return 0;
-}
-if (letters == 0)
-{
-fclose(file);
-fprintf(stderr, "Zero characters requested\n");
+if (!filename)
 return (0);
-}
-buff = malloc(letters + 1);
-if (buff == NULL)
-{
-fclose(file);
-fprintf(stderr, "Memory allocation error\n");
+
+fd = open(filename, O_RDONLY);
+
+if (fd == -1)
 return (0);
-}
-bytesR = fread(buff, 1, letters, file);
-if (bytesR < 0)
-{
-free(buff);
-fclose(file);
-fprintf(stderr, "Read error\n");
+
+buf = malloc(sizeof(char) * (letters));
+if (!buf)
 return (0);
-}
-if (bytesR == 0)
-{
-fclose(file);
-fprintf(stderr, "File is empty or read error\n");
-return (0);
-}
-fclose(file);
-buff[bytesR] = '\0';
-fprintf(stderr, "%s", buff);
-free(buff);
-return (bytesR);
+
+nrd = read(fd, buf, letters);
+nwr = write(STDOUT_FILENO, buf, nrd);
+
+close(fd);
+
+free(buf);
+
+return (nwr);
 }
